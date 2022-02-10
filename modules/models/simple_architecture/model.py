2fc66222db92df3457630b38dfc9cde0f9d1701a
Ekaterina Sedykh <58919166+katesedykh@users.noreply.github.com>
2022-02-10 22:33:13 +0200

    remove corrector

--- modules/models/simple_architecture/model.py
+++ modules/models/simple_architecture/model.py
@@ -71,15 +71,15 @@ def rescale_prediction(pred):
 
 class SimpleCharRNN(nn.Module):
 
-    def __init__(self, input_size, output_size, hidden_dim, n_layers, device, bilstm=False, corrector=None):
+    def __init__(self, input_size, output_size, hidden_dim, n_layers, device, bilstm=False):
         super(SimpleCharRNN, self).__init__()
         self.bilstm = bilstm
         self.device = device
         if self.bilstm:
-            self.model_forward = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device, corrector)
-            self.model_backward = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device, corrector)
+            self.model_forward = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device)
+            self.model_backward = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device)
         else:
-            self.model = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device, corrector)
+            self.model = SimpleCharRNNUnit(input_size, output_size, hidden_dim, n_layers, device)
 
     def reverse_tensor(self, x):
         x = x.permute(1, 0, 2)
@@ -88,12 +88,6 @@ class SimpleCharRNN(nn.Module):
         inverted_tensor = x.index_select(0, idx)
         inverted_tensor = inverted_tensor.permute(1, 0, 2)
         return inverted_tensor
-
-    def use_corrector(self, use):
-        if self.bilstm:
-            self.model_forward.use_corrector(use)
-        else:
-            self.model.use_corrector(use)
 
     def forward(self, x, lengths, hiddens=None):
         if self.bilstm:
@@ -115,12 +109,9 @@ class SimpleCharRNNUnit(nn.Module):
         self.n_layers = n_layers
         self.device = device
 
-        self.corrector = corrector if corrector is not None else rescale_prediction
-
+  
         self.lstmcell = nn.LSTMCell(input_size + 9, hidden_dim)
         self.h2o = nn.Linear(hidden_dim, output_size)
-        self.corrector_i = 0
-        self.use_corrector_flag = False
 
     def init_hiddens(self, batch):
         h0 = torch.zeros(batch, self.hidden_dim).requires_grad_()
@@ -131,23 +122,8 @@ class SimpleCharRNNUnit(nn.Module):
         nn.init.kaiming_uniform_(c0)
         return (h0, c0)
 
-    def use_corrector(self, use):
-        self.use_corrector_flag = use
-
     def init_preds(self, batch):
         return torch.zeros((batch, self.output_size))
-
-    def correct_coordinates(self, coordinates, finished):
-        coordinates_l = list(coordinates.unbind(0))
-        for i, c in enumerate(coordinates_l):
-            c = c.view(-1, 3)
-            if i not in finished:
-                coordinates_l[i] = self.corrector(c)
-            else:
-                coordinates_l[i] = c
-        coordinates = torch.stack(coordinates_l)
-
-        return coordinates
 
     def forward(self, x, lengths, hiddens=None, y=None):
         batch_size = x.shape[0]
@@ -169,17 +145,6 @@ class SimpleCharRNNUnit(nn.Module):
             output_i = self.h2o(h)
             if y is not None:
                 output_i = (output_i + y[:, i, :]) / 2
-            if self.use_corrector_flag and not self.training:
-                if output_i_prev is not None:
-                    last_two = torch.cat((output_i_prev.unsqueeze(1), output_i.unsqueeze(1)), dim=1)
-                    _, output_i = self.correct_coordinates(last_two,
-                                                           np.arange(len(lengths.cpu()))[lengths.cpu() <= i]) \
-                        .chunk(2, 1)
-                    output_i[i >= lengths] = torch.zeros_like(output_i[0])
-                    output_i = output_i.view(-1, 9)
-                else:
-                    output_i = self.correct_coordinates(output_i.unsqueeze(1), np.array([]))
-                    output_i = output_i.view(-1, 9)
             output_i_prev = output_i.view(-1, 9).clone()
             
             if i == 0:
