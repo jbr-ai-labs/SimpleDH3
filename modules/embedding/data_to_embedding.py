3b437715044c4af05aa87c3e874fa1c3cc767f66
Ekaterina Sedykh <58919166+katesedykh@users.noreply.github.com>
2022-04-22 13:46:29 +0300

    update test embeddings

--- modules/embedding/data_to_embedding.py
+++ modules/embedding/data_to_embedding.py
@@ -16,14 +16,32 @@ if __name__ == '__main__':
             seq = get_embeddings(config["PATH_TO_PRETRAINED_EMBEDDING_MODEL"], config["PATH_TO_SEQ_DATA"])
             with open(config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_light', "wb") as seq_file:
                 pickle.dump(seq, seq_file, protocol=pickle.DEFAULT_PROTOCOL)
+                
+            seq_test = get_embeddings(config["PATH_TO_PRETRAINED_EMBEDDING_MODEL"], config["PATH_TO_SEQ_DATA_TEST"])
+            with open(config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_test_light', "wb") as seq_file_test:
+                pickle.dump(seq_test, seq_file_test, protocol=pickle.DEFAULT_PROTOCOL)
+                
         elif config["EMBEDDINGS_NAME"] == EMBEDDINGS_NAMES[1]:
             seq = get_embeddings_test(config["PATH_TO_SEQ_DATA"])
             with open(config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_normal', "wb") as seq_file:
                 pickle.dump(seq, seq_file, protocol=pickle.DEFAULT_PROTOCOL)
+                
+            seq_test = get_embeddings_test(config["PATH_TO_SEQ_DATA_TEST"])
+            with open(config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_test_normal', "wb") as seq_file_test:
+                pickle.dump(seq_test, seq_file_test, protocol=pickle.DEFAULT_PROTOCOL)
+                
         elif config["EMBEDDINGS_NAME"] == EMBEDDINGS_NAMES[2]:
             seq = get_embeddings_esm(config["PATH_TO_SEQ_DATA"])
             torch.save(seq, config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_esm')
+            
+            seq_test = get_embeddings_esm(config["PATH_TO_SEQ_DATA_TEST"])
+            torch.save(seq, config["PATH_TO_SEQ_EMBEDDED"] + 'cdr_h3_seq_test_esm')
+                        
         coord = get_coordinates(config["PATH_TO_COORD_DATA"])
         with open(config["PATH_TO_COORD_EMBEDDED"], "wb") as coord_file:
             pickle.dump(coord, coord_file, protocol=pickle.HIGHEST_PROTOCOL)
+            
+        coord_test = get_coordinates(config["PATH_TO_COORD_DATA_TEST"])
+        with open(config["PATH_TO_COORD_EMBEDDED"] + 'test', "wb") as coord_file_test:
+            pickle.dump(coord_test, coord_file_test, protocol=pickle.HIGHEST_PROTOCOL)
 
